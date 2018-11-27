c3b2297dd5c9377cd33104caa3d72bf80d8efcea
lukaszsl <lukasz.sliczner@interia.pl>
2018-11-27 21:41:57 +0000

    Create single campus page
    .

--- app/public/wp-content/themes/fictional-university -theme/archive-campus.php
+++ app/public/wp-content/themes/fictional-university -theme/archive-campus.php
@@ -9,13 +9,15 @@
 <div class="container container--narrow page-section">
 
 	<div class="acf-map">
-		<?php while(have_posts()): ?>
-			<?php the_post(); ?>
-			<?php $mapLocation = get_field('map_location'); ?>
-			<div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng']; ?>"></div>
-		<?php endwhile; ?>
-
-		<?php echo paginate_links(); ?>
+		<?php while(have_posts()) {
+			the_post();
+			$mapLocation = get_field('map_location');
+			?>
+			<div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng']; ?>">
+				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+				<?php echo $mapLocation['address']; ?>
+			</div>
+		<?php } ?>
 	</div>
 
 </div>
--- app/public/wp-content/themes/fictional-university -theme/single-program.php
+++ app/public/wp-content/themes/fictional-university -theme/single-program.php
@@ -79,6 +79,20 @@
 					}
 				}
 
+				wp_reset_postdata();
+				$relatedCampuses = get_field('related_campus');
+
+				echo '<hr class="section-break">';
+				if($relatedCampuses) {
+					echo '<h2 class="headline headline--medium">' . get_the_title() . ' is available at this Campuses: </h2>';
+
+					echo '<ul class="min-list link-list">';
+					foreach($relatedCampuses as $campus) {
+						?>  <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus); ?></a></li> <?php
+					}
+					echo '</ul>';
+				}
+
 			?>
 
 		</div>
