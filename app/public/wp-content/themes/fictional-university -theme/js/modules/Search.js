556111862b66ea771afbae5242aa990da2e3dab9
lukaszsl <lukasz.sliczner@interia.pl>
2018-12-09 20:52:12 +0000

    Move search bar html to search.js

--- app/public/wp-content/themes/fictional-university -theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university -theme/js/modules/Search.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 class Search {
 	// create and describe object
 	constructor() {
+		this.addSearchHTML(),
 		this.resultsDiv = $("#search-overlay__results");
 		this.openButton = $(".js-search-trigger");
 		this.closeButton = $(".search-overlay__close");
@@ -32,7 +33,7 @@ class Search {
 					this.resultsDiv.html('<div class="spinner-loader"></div>');
 					this.isSpinnerVisible = true;
 				}
-				this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+				this.typingTimer = setTimeout(this.getResults.bind(this), 750);
 			} else {
 				this.resultsDiv.html('');
 				this.isSpinnerVisible = false;
@@ -65,6 +66,9 @@ class Search {
 	openOverlay() {
 		this.searchOverlay.addClass("search-overlay--active");
 		$("body").addClass("body-no-scroll");
+		this.searchField.val('');
+		this.resultsDiv.html('');
+		setTimeout(() => this.searchField.focus(), 301);
 		this.isOverlayOpen = true;
 	}
 
@@ -72,6 +76,23 @@ class Search {
 		this.searchOverlay.removeClass("search-overlay--active");
 		$("body").removeClass("body-no-scroll");
 		this.isOverlayOpen = false;
+	}
+
+	addSearchHTML() {
+		$("body").append(`
+			<div class="search-overlay">
+				<div class="search-overlay__top">
+					<div class="container">
+						<i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+						<input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+						<i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="container">
+					<div id="search-overlay__results"></div>
+				</div>
+			</div>
+		`);
 	}
 
 }
