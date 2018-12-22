96aaa522a03fcf41301f94c6927d9c68d39a53a9
lukaszsl <lukasz.sliczner@interia.pl>
2018-12-22 21:12:40 +0000

    Add non-js fallback search

--- app/public/wp-content/themes/fictional-university -theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university -theme/js/modules/Search.js
@@ -119,6 +119,7 @@ class Search {
     setTimeout(() => this.searchField.focus(), 301);
     console.log("our open method just ran!");
     this.isOverlayOpen = true;
+		return false;
   }
 
   closeOverlay() {
