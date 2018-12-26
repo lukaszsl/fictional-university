b45ed7c2b809122839cc371b30d34520b6b3b064
lukaszsl <lukasz.sliczner@interia.pl>
2018-12-26 21:13:58 +0000

    Add plugin members

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -3,6 +3,8 @@ function university_post_types() {
 
 	//Campus post type
 	register_post_type('campus', array(
+		'capability_type' => 'campus',
+		'map_meta_cap' => true,
 		'supports' => array('title', 'editor', 'excerpt'),
 		'rewrite' => array('slug' => 'campuses'),
 		'has_archive' => true,
@@ -19,6 +21,8 @@ function university_post_types() {
 
 	//Event post type
 	register_post_type('event', array(
+		'capability_type' => 'event',
+		'map_meta_cap' => true,
 		'supports' => array('title', 'editor', 'excerpt'),
 		'rewrite' => array('slug' => 'events'),
 		'has_archive' => true,
--- app/sql/local.wp_users.sql
+++ app/sql/local.wp_users.sql
@@ -2,4 +2,5 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_users` VALUES
-(1,"Lukas","$P$Bm.iw0p86LWnlzX7h4If3vLrK9j4rQ1","lukas","marichuan@interia.pl","","2018-10-07 21:16:38","",0,"Lukas");
+(1,"Lukas","$P$Bm.iw0p86LWnlzX7h4If3vLrK9j4rQ1","lukas","marichuan@interia.pl","","2018-10-07 21:16:38","",0,"Lukas"),
+(2,"marichuan","$P$BJBf73c9Wg4PHmOeEmk/oozWqOr2Iu1","marichuan","marichuan1@interia.pl","","2018-12-26 20:42:38","1545856958:$P$BXktzyW87uWtnVLgaHfhd0/oYmSt.U1",0,"marichuan");
