e0bbe3be26008893c64f18082fd0e0b8c97fb705
lukaszsl <lukasz.sliczner@interia.pl>
2019-01-02 20:46:55 +0000

    Create open registration

--- app/sql/local.wp_users.sql
+++ app/sql/local.wp_users.sql
@@ -3,4 +3,5 @@
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_users` VALUES
 (1,"Lukas","$P$Bm.iw0p86LWnlzX7h4If3vLrK9j4rQ1","lukas","marichuan@interia.pl","","2018-10-07 21:16:38","",0,"Lukas"),
-(2,"marichuan","$P$BJBf73c9Wg4PHmOeEmk/oozWqOr2Iu1","marichuan","marichuan1@interia.pl","","2018-12-26 20:42:38","1545856958:$P$BXktzyW87uWtnVLgaHfhd0/oYmSt.U1",0,"marichuan");
+(2,"marichuan","$P$BJBf73c9Wg4PHmOeEmk/oozWqOr2Iu1","marichuan","marichuan1@interia.pl","","2018-12-26 20:42:38","1545856958:$P$BXktzyW87uWtnVLgaHfhd0/oYmSt.U1",0,"marichuan"),
+(3,"kittydoe","$P$BSPyzu2s4u3PQA0S8Us2mnK11fEflf/","kittydoe","kittydoe@kittydoe.vl","","2018-12-27 21:00:08","1545944408:$P$BaFBh1bMxuP7LHfF71ydsgppzoDDTo0",0,"kittydoe");
