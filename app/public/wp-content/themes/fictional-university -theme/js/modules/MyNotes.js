0e4261bafcae915eab2d6421175defa38b2937b1
lukaszsl <lukasz.sliczner@interia.pl>
2019-01-15 21:11:18 +0000

    Set user post limit

--- app/public/wp-content/themes/fictional-university -theme/js/modules/MyNotes.js
+++ app/public/wp-content/themes/fictional-university -theme/js/modules/MyNotes.js
@@ -50,6 +50,9 @@ class MyNotes {
 				thisNote.slideUp();
 				console.log("Congrats");
 				console.log(response);
+				if (response.userNoteCount < 5) {
+					$(".note-limit-message").removeClass("active");
+				}
 			},
 			error: (response) => {
 				console.log("Sorry");
@@ -115,6 +118,9 @@ class MyNotes {
 				console.log(response);
 			},
 			error: (response) => {
+				if (response.responseText == "You have reached your note limit.") {
+					$(".note-limit-message").addClass("active");
+				}
 				console.log("Sorry");
 				console.log(response);
 			}
