1605281ab1001e964293bdb9fff3c80609253c88
lukaszsl <lukasz.sliczner@interia.pl>
2019-01-18 21:03:31 +0000

    Create custom POST and DELETE endpoints

--- app/public/wp-content/themes/fictional-university -theme/js/modules/Like.js
+++ app/public/wp-content/themes/fictional-university -theme/js/modules/Like.js
@@ -1,19 +1,17 @@
 import $ from 'jquery';
 
-
 class Like {
 	constructor() {
 		this.events();
 	}
 
 	events() {
-		$(".like-box").on("click", this.ourClickDispacher.bind(this));
+		$(".like-box").on("click", this.ourClickDispatcher.bind(this));
 	}
 
 	// methods
-	ourClickDispacher(e) {
-
-		var currentLikeBox = $(e.target).closest("like-box");
+	ourClickDispatcher(e) {
+		var currentLikeBox = $(e.target).closest(".like-box");
 
 		if (currentLikeBox.data('exists') == 'yes') {
 			this.deleteLike();
@@ -23,13 +21,30 @@ class Like {
 	}
 
 	createLike() {
-		alert("create test message");
+		$.ajax({
+			url: universityData.root_url + '/wp-json/university/v1/manageLike',
+			type: 'POST',
+			success: (response) => {
+				console.log(response);
+			},
+			error: (response) => {
+				console.log(response);
+			}
+		});
 	}
 
 	deleteLike() {
-		alert("delete test message");
+		$.ajax({
+			url: universityData.root_url + '/wp-json/university/v1/manageLike',
+			type: 'DELETE',
+			success: (response) => {
+				console.log(response);
+			},
+			error: (response) => {
+				console.log(response);
+			}
+		});
 	}
-
 }
 
 export default Like;
